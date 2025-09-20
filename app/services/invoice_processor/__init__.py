4fe32668656a8aa9fd4d2f766fe101d9bea8081f
egoldin <goldin.evg@gmail.com>
2025-09-20 13:57:04 +0300

    feat: updated InvoiseType values to fit splitwise schema

--- app/services/invoice_processor/__init__.py
+++ app/services/invoice_processor/__init__.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
 class InvoiceType(Enum):
-    WATER = "water"
-    ELECTRICITY = "electricity"
-    GAS = "gas"
-    INTERNET = "internet"
-    PROPERTY_TAX = "property_tax"
-    UNSUPPORTED = "unsupported"
+    WATER = 7
+    ELECTRICITY = 5
+    GAS = 6
+    INTERNET = 8
+    PROPERTY_TAX = 45
+    UNSUPPORTED = 0
